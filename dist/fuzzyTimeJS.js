68ebfc39bcb763e3c005182bae7e140295b2234e
Muhammed Saho <muhasaho@gmail.com>
2015-06-28 16:18:24 -0500

    initial structure

---
+++ dist/fuzzyTimeJS.js
@@ -0,0 +1,23 @@
+(function(root, undefined) {
+
+  "use strict";
+
+
+/* fuzzyTimeJS main */
+
+// Base function.
+var fuzzyTimeJS = function() {
+  // Add functionality here.
+  return true;
+};
+
+
+// Version.
+fuzzyTimeJS.VERSION = '0.0.0';
+
+
+// Export to the root, which is probably `window`.
+root.fuzzyTimeJS = fuzzyTimeJS;
+
+
+}(this));
---
+++ src/main.js
@@ -0,0 +1,15 @@
+/* fuzzyTimeJS main */
+
+// Base function.
+var fuzzyTimeJS = function() {
+  // Add functionality here.
+  return true;
+};
+
+
+// Version.
+fuzzyTimeJS.VERSION = '0.0.0';
+
+
+// Export to the root, which is probably `window`.
+root.fuzzyTimeJS = fuzzyTimeJS;
