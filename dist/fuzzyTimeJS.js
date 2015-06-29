79bf2d7387ff0a2bee5a7023738216c2b59f6bf0
Muhammed Saho <muhasaho@gmail.com>
2015-06-28 21:27:14 -0500

    test getMinuteWord

--- dist/fuzzyTimeJS.js
+++ dist/fuzzyTimeJS.js
@@ -5,9 +5,27 @@
 
 /* fuzzyTimeJS main */
 
-// Base function.
+// Base Object.
 var fuzzyTimeJS = {
   vars : {
+    // Define minute map
+    minuteMap : {
+      "0": "o'clock",
+      "5": "five",
+      "10": "ten",
+      "15": "quarter",
+      "20": "twenty",
+      "30": "half"
+    },
+    
+    // Define infix map
+    infixMap : {
+      "to": " to ",
+      "past": " past ",
+      "nothing": " "
+    },
+    
+    // Define hour map
     hourMap : [
       "twelve",
       "one",
@@ -24,26 +42,44 @@ var fuzzyTimeJS = {
       "twelve"
     ],
     
-    infixMap : {
-      "to": " to ",
-      "past": " past ",
-      "nothing": " "
-    },
-    
-    
-    minuteMap : {
-      "0": "o'clock",
-      "5": "five",
-      "10": "ten",
-      "15": "quarter",
-      "20": "twenty",
-      "30": "half"
-    },
-    
-    
+    // Define postfix map
     postfixMap : {
       "sharp": "o'clock",
       "nothing": ""
+    }
+  },
+  
+  helpers: {
+    /**
+     * returns the word corresponding to minute past or to
+     */
+    getMinuteWord: function(minutesPast){
+      var minuteMap = fuzzyTimeJS.vars.minuteMap;
+      
+      // The minutesPast is "to"
+      if (minutesPast > 33){
+        minutesPast = 60 - minutesPast;
+      }
+      
+      // return appropriate minute word
+      if(minutesPast <= 3){
+        return minuteMap["0"];
+      }
+      else if(minutesPast <= 7){
+        return minuteMap["5"];
+      }
+      else if(minutesPast <= 12){
+        return minuteMap["10"];
+      }
+      else if(minutesPast <= 17){
+        return minuteMap["15"];
+      }
+      else if(minutesPast <= 26){
+        return minuteMap["20"];
+      }
+      else{
+        return minuteMap["30"];
+      }
     }
   },
     
@@ -52,21 +88,10 @@ var fuzzyTimeJS = {
       hour = typeof hour !== 'undefined' ? hour : 0;
       minute = typeof minute !== 'undefined' ? minute : "0";
       
+      
       return this.vars.hourMap[hour] + this.vars.infixMap.nothing + this.vars.minuteMap[minute] + this.vars.postfixMap.nothing;
     }
 };
-  // Define hour map
-
-  
-  // Define infix map
-
-  
-  // Define minute map
-
-  
-  // Define postfix map
-
-
 
 // Version.
 fuzzyTimeJS.VERSION = '0.0.1';
--- test/all.js
+++ test/all.js
@@ -1,11 +1,36 @@
 /// <reference path="lib/qunit.js" />
 
-test("the base function exists", function() {
+test("the base object exists", function() {
   ok(fuzzyTimeJS);
 });
 
 test("no arguments return \"twelve o'clock\"", function(assert){
   assert.equal(fuzzyTimeJS.convert(), "twelve o'clock");
+});
+
+test("getMinuteWord", function(assert) {
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(0), "o'clock");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(3), "o'clock");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(4), "five");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(7), "five");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(8), "ten");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(12), "ten");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(13), "quarter");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(17), "quarter");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(18), "twenty");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(26), "twenty");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(27), "half");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(33), "half");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(34), "twenty");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(42), "twenty");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(43), "quarter");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(47), "quarter");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(48), "ten");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(52), "ten");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(53), "five");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(56), "five");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(57), "o'clock");
+  assert.equal(fuzzyTimeJS.helpers.getMinuteWord(59), "o'clock");
 });
 
 //test("twelve", window.fuzzyTimeJS());
